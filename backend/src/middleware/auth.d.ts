cf62fa828909cef9f54902e0aab0a7b36159a2af
Kavya Shree T V <kavyaselvam1127@gmail.com>
2026-07-24 20:24:54 +0530

    clean: remove compiled JS and declaration files from src

--- backend/src/middleware/auth.d.ts
+++
@@ -1,3 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export declare const protect: (req: Request, res: Response, next: NextFunction) => Promise<void>;
-//# sourceMappingURL=auth.d.ts.map
